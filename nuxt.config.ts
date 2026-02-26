9ee0b73cf3402a54d0b654492afd7d8a57319481
sjnosukequy <94581993+sjnosukequy@users.noreply.github.com>
2026-02-26 14:25:24 +0700

    modify the github deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/ui",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/eslint",
-    "@nuxt/content",
-    "nuxt-easy-lightbox",
-  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/scripts", "@nuxt/eslint", "@nuxt/content", "nuxt-easy-lightbox"],
   css: ["~/assets/css/main.css"],
   app: {
     head: {
-      title: 'Sjnosukequy - Vuong Dinh Quy', // default fallback title
+      title: "Sjnosukequy - Vuong Dinh Quy", // default fallback title
       htmlAttrs: {
-        lang: 'en',
+        lang: "en",
       },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
-      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
     },
   },
 });
